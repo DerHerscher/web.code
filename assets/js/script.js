2c40141dc75490550e1d9a11881905ea534db341
lars <schroett4388@mailbox.org>
2022-12-08 15:31:23 +0100

    Aufgabe2

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,31 @@ function Podcast( titel, beschreibung, autor, besitzerName, besitzerEmail, bildU
 
 let lnp = new Podcast("LNP", "Voll toller Podcast", "Tim", "Tim Pritlove", "pritlove@lnp.de", "www.help.com", "feedme.de", ["Politk", "Netgedöns"]);
 console.log(lnp);
+
+function Episode(titel, beschreibung, dauer){
+    this.titel = titel,
+    this.beschreibung = beschreibung,
+    this.dauer = dauer,
+    this.datum = new Date();
+}
+
+//Funktion für Objekt Episode
+Episode.prototype.getDauerInStundenUndMinuten = function() {
+    let min = Math.floor((this.dauer/1000)/60);
+    let h = Math.floor(((this.dauer/1000)/60)/60);
+    min = min - (h*60);
+    return `${h}h ${min}min`;
+}
+
+function EpisodeAudio(url, groesse, typ, episode){
+    this.url = url,
+    this.groesse = groesse,
+    this.typ = typ;
+    this.episode = episode;
+}
+
+let folge100 = new Episode("Dönnerstag am Mittwoch", "Achtung der Dönerstag wurde verlegt", 5880000 );
+let folge100Datei = new EpisodeAudio("www.audio.datei/daei", 48987, "mp3", folge100);
+console.log(folge100);
+console.log(folge100.getDauerInStundenUndMinuten());
+console.log(folge100Datei);
