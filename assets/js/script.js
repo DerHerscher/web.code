734fce64580643ac3a09d8d2dd4d8baea36e66b2
lars <schroett4388@mailbox.org>
2022-12-08 14:45:43 +0100

    Aufgabe1

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,18 @@ console.log(`Die Viewport-Breite beträgt: ${getViewportWidth()} Pixel.`);
 if( getViewportWidth() < screen.width*0.3) {
     alert("Achtung Achtung Sie verlassen den gesicherten Bereich");
 }
+
+function Podcast( titel, beschreibung, autor, besitzerName, besitzerEmail, bildUrl, feedUrl, kategorien ){
+    this.titel = titel,
+    this.beschreibung = beschreibung,
+    this.autor = autor,
+    this.besitzerName = besitzerName,
+    this.besitzerEmail = besitzerEmail,
+    this.bildUrl = bildUrl,
+    this.feedUrl = feedUrl,
+    this. kategorien = kategorien,
+    this.letztesUpdate = new Date();    //Fragen ob Date Objekt oder Zeit und Datum als String gespeichert werden soll?
+}
+
+let lnp = new Podcast("LNP", "Voll toller Podcast", "Tim", "Tim Pritlove", "pritlove@lnp.de", "www.help.com", "feedme.de", ["Politk", "Netgedöns"]);
+console.log(lnp);
