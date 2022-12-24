021854d110a9b6b842714fc989574f545d4b0637
lars <schroett4388@mailbox.org>
2022-12-24 14:23:46 +0100

    PR10 nach Aufgabe1

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,8 +52,35 @@ function EpisodeAudio(url, groesse, typ, episode){
 
 //------------------------------------------------------------------------------------------------------------------------------------
 //Beispiel Objekte und Ausgaben
-let lage = new Podcast("Lage der Nation", "Ein Politik Podcast", "Ulf Burmeyer", "Phillip Banse","support@lagedernation.org","assets/img/2560px-Lage_der_Nation_Logo.svg","feed.com",["Politk","news"], new Date("1999-10-04T03:24:00"));
-let lnp = new Podcast("LNP", "Voll toller Podcast", "Tim", "Tim Pritlove", "pritlove@lnp.de", "www.help.com", "feedme.de", ["Politk", "Netgedöns"],new Date("1995-12-17T03:24:00"));
+let lage = new Podcast("Lage der Nation",
+                        "Der Politik-Podcast aus Berlin mit Phillip Banse und Ulf Buermeyer ",
+                        "Phillip Banse, Ulf Buermeyer",
+                        "Phillip Banse",
+                        "support@lagedernation.org",
+                        "assets/img/2560px-Lage_der_Nation_Logo.svg",
+                        "podcast.html",
+                        ["Politk","news"],
+                         new Date("1999-10-04T03:24:00"));
+
+let lnp = new Podcast("LNP",
+                        "Logbuch:Netzpolitik ist ein wöchentlicher Podcast über das netzpolitische Geschehen mit Linus Neumann und Tim Pritlove.",
+                        "Tim Pritlove, Linus Neumann",
+                        "Tim Pritlove",
+                        "lnp@metaebene.me",
+                        "assets/img/logbuchnetzpolitik.jpeg",
+                        "https://logbuch-netzpolitik.de/feed/m4a",
+                        ["Politk", "Netgedöns"],
+                        new Date("1995-12-17T03:24:00"));
+
+let chaosradio = new Podcast("Chaosradio",
+                        "In Chaosradio besprechen Menschen aus dem CCC und seinem Umfeld seit 1995 jeden Monat technische und gesellschaftliche Themen.",
+                        "Marcus Richter",
+                        "Chaos Computer Club",
+                        "chaosradio@ccc.de",
+                        "assets/img/chaosradio_500x.png",
+                        "https://chaosradio.de/feed/m4a",
+                        ["Politk", "Netzgedöns"],
+                        new Date("1992-11-14T14:12:23"));
 
 let folge306 = new Episode("Lage der Nation 306", "Putins Eskalation, Gas-Pipelines explodiert, Proteste im Iran (Interview Gilda Sahebi, taz), 200 Mrd. für Energiehilfen, Energiemarkt der Zukunft (Interview Lion Hirth, Hertie School), Umgang mit Bad News", 4980000,new Date("2020-12-17T03:24:00"));
 let folge307 = new Episode("Lage der Nation 307", "49-Euro-Ticket, AKW-Streit, Niedersachsen-Wahl, Strategie für die FDP, russischer Staatsterror, Risiko eines Atomwaffeneinsatzes399", 5880000,new Date("2022-02-01T03:24:00"));
@@ -65,7 +92,7 @@ let folge101 = new Episode("Dönerstag wieder am Donnerstag", "Blah", 5880000)
 lnp.addEpisode(folge101);
 lnp.addEpisode(folge100);
 
-let podcastArray = [lage, lnp];
+let podcastArray = [lage, lnp, chaosradio];
 
 podcastArray.forEach( pod => {
     console.log(`${pod.titel} (Letztes Update: ${pod.letztesUpdate}):`);
