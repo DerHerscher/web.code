2cb9cc9e163d26fa4acd9b70f1ec4a88ecc52b16
lars <schroett4388@mailbox.org>
2023-01-11 17:03:27 +0100

    changed test tp be better

--- v1/test/test.js
+++ v1/test/test.js
@@ -3,7 +3,7 @@ const data = require("../models/persistence.js");
 
 
 data.abonnieren("https://feeds.lagedernation.org/feeds/ldn-mp3.xml", () => {
-    data.abonnieren("https://feeds.metaebene.me/lnp/m4a", server)
+    data.abonnieren("https://feeds.metaebene.me/lnp/m4a", () => {console.log("Podcast abonniert");} )
 });
 
 // data.abonnieren("https://wowirsindistvorne.show/feed/mp3/", () => {
@@ -12,8 +12,15 @@ data.abonnieren("https://feeds.lagedernation.org/feeds/ldn-mp3.xml", () => {
 
 
 //Server Funktion die eine Webserver und den HTML Content erstellt.
-function server(){
+    let server = http.createServer(function(request, response){
+        response.writeHead(200, {"content-type" : "text/html; charset=utf-8"})
+        response.end(createContent());
+    });
+    server.listen(8844, function() {
+        console.log("Der Server läuft auf http://localhost:8844");
+    });
 
+let createContent = function(){
     let content = "";
     data.podcasts.forEach( pod =>{
         content += `<hr>
@@ -25,10 +32,9 @@ function server(){
             content += `<li>${pod.episoden[i].titel}</li>`;
         }
         content += `</ul>`;
-
     });
 
-let html = `<!DOCTYPE html>
+    let html = `<!DOCTYPE html>
     <html lang="de">
         <head>
             <title>Podcast-App Test</title>
@@ -42,22 +48,5 @@ let html = `<!DOCTYPE html>
         <body>
     </html>`;
 
-    
-    let server = http.createServer(function(request, response){
-        response.writeHead(200, {"content-type" : "text/html; charset=utf-8"})
-        response.end(html);
-    });
-    server.listen(8844, function() {
-        console.log("Der Server läuft auf http://localhost:8844");
-    });
+    return html;
 }
-    
-
-//Lokale Tests
-
-// function testlog() {
-//   console.log(data.podcasts[0]);
-// }
-
-// data.abonnieren("https://chaosradio.de/feed/m4a", testlog)
-// data.abonnieren("https://feeds.lagedernation.org/feeds/ldn-mp3.xml", testlog)
